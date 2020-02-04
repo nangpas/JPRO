e05cf51707c0d56481e32284d3777e67c29aee8d
nangpas <nangpas@naver.com>
2020-02-04 17:52:10 +0900

    이미지 src 추출 기능 완료

--- src/main/java/com/nyc/shop/web/ShopController.java
+++ src/main/java/com/nyc/shop/web/ShopController.java
@@ -2,7 +2,9 @@ package com.nyc.shop.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,16 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -122,29 +118,68 @@ public class ShopController {
 	/**
      * 테스트 페이지
      * @return string
+	 * @throws org.json.simple.parser.ParseException 
      */
-	@RequestMapping(value = "/jpro/test.do")
-	public String test(HttpServletRequest request, ModelMap model, @RequestParam Map<String, Object> commandMap) {
-		String url = "https://www.google.co.kr/search?q=%ED%86%B0%EC%BA%A3&hl=ko&sxsrf=ACYBGNRPPVThdcfd1esObhsdZuI9iOydnQ:1580707260039&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjy0um10bTnAhXJy4sBHX-UC9UQ_AUoAXoECBIQAw&biw=1920&bih=1057";
-		//String url = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=나이키";
+	@ResponseBody
+	@RequestMapping(value = "/jpro/testCrawling.do")
+	public List<String> testCrawling(HttpServletRequest request, ModelMap model, @RequestParam Map<String, Object> commandMap){
+		
+		// can only grab first 100 results
+		String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+		String url = "https://www.google.com/search?q=" + commandMap.get("KEYWARD") + "&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiuzIj3tLfnAhWTOnAKHY6RBzMQ_AUoAXoECAwQAw&biw=1920&bih=1057";
+		List<String> result = new ArrayList<String>();
+		
 		try {
-			System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			
-			driver.get(url);
-			//
-			List<WebElement> element = null;
-			element = driver.findElements(By.className("rg_l"));
-			for(int i = 0; i < element.size(); i++) {
-				logger.debug(element.get(i).getAttribute("innerHTML"));
-			}
-			
-			//logger.debug(driver.getPageSource());
-		} catch (Exception e) {
+		    Document doc = Jsoup.connect(url).userAgent(userAgent).referrer("https://www.google.com/").get();
+		    Elements elements = doc.select("img.rg_i");
+		    
+		    for (Element element : elements) {
+		    	result.add(element.attr("data-iurl"));
+		    }
+		} catch (IOException ie) {
+			logger.debug(ie.getMessage());
+		} catch(Exception e){
 			logger.debug(e.getMessage());
 		}
 		
-		
+		return result;
+	}
+	
+	
+	/**
+     * 테스트 페이지
+     * @return string
+	 * @throws org.json.simple.parser.ParseException 
+     */
+	@RequestMapping(value = "/jpro/test.do")
+	public String test(HttpServletRequest request, ModelMap model, @RequestParam Map<String, Object> commandMap) throws org.json.simple.parser.ParseException {
 		return "jpro/test";
 	}
+	
+	/*
+	String url = "https://www.google.co.kr/search?q=%ED%86%B0%EC%BA%A3&hl=ko&sxsrf=ACYBGNRPPVThdcfd1esObhsdZuI9iOydnQ:1580707260039&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjy0um10bTnAhXJy4sBHX-UC9UQ_AUoAXoECBIQAw&biw=1920&bih=1057";
+	WebDriver driver = null;
+	
+	try {
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "/chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get(url);
+		//
+		List<WebElement> element = null;
+		element = driver.findElements(By.className("rg_i"));
+		for(int i = 0; i < element.size(); i++) {
+			logger.debug(element.get(i).getAttribute("innerHTML"));
+			logger.debug(element.get(i).getAttribute("src"));
+		}
+		
+		model.addAttribute("text", element.get(1).getAttribute("src"));
+		
+	} catch (Exception e) {
+		logger.debug(e.getMessage());
+	}finally {
+		driver.close();
+	}
+	*/
 }
